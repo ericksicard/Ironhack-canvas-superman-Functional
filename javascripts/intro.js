893d9846189c7de8219af3414e87c47c51961410
Erick Sicard <esrfree@gmail.com>
2019-11-22 17:40:15 -0500

    fireballs moving

--- javascripts/intro.js
+++ javascripts/intro.js
@@ -1,54 +1,73 @@
-var canvas = document.getElementById('example');
-var ctx = canvas.getContext('2d');
 
-// ****************************
-// draw rectangle:
-// ****************************
-function draw() {  
-      ctx.fillRect(25, 25, 100, 100);
-      ctx.clearRect(45, 45, 60, 60);
-      ctx.strokeRect(50, 50, 50, 50);
+class Component {
+  constructor( img, x, y, w, h) {
+    this.imgScr = img;
+    this.posX = x;
+    this.posY = y;
+    this.imgW = w;
+    this.imgH = h
   }
+};
 
-  draw()
+let canva = {
+  width: 500,
+  height: 500
+};
 
-ctx.fillStyle="purple";
-ctx.fillRect(260, 260, 30, 30);
+function init() {
+  // insert canvas HTML
+  let canvasHTML = `<canvas id="field" width=${canva.width} height=${canva.height}></canvas>`
+  document.querySelector('body').insertAdjacentHTML('afterBegin', canvasHTML);  
+  // set canvas context
+  let ctx = document.getElementById('field').getContext('2d');
+  
+  // new object instances
+  let superman = new Component( "./img/superman.png", 0, 180, 100, 150 );
+  let fireball = new Component( "./img/fireball.png", 450, 200, 50, 50 );  
+  
+  // draw components
+  background( ctx );
+  drwaComp( ctx, superman );
+  drwaComp( ctx, fireball );
 
-// ****************************
-// draw path
-// ****************************
-// start the path
-ctx.beginPath();
-// starting position is x=50, y=50
-ctx.moveTo(50, 200);
-// draw the line that has final coordinates x=250, y=50
-ctx.lineTo(250, 200);
-// .stroke() executes the drawing
-ctx.stroke();
-// start a new line from these coordinates: x=250, y=50
-ctx.moveTo(250, 200);
-// draw the line that has final coordinates x=250, y=100
-ctx.lineTo(250, 250);
-// .stroke() executes the drawing
-ctx.stroke();
-// close the path
-ctx.closePath();
+  animation( ctx, superman, fireball)
+}
 
-// ****************************
-// draw circle
-// ****************************
-ctx.beginPath();
-// ctx.arc(x, y, radius, startAngle, endAngle)
-ctx.arc(150, 170, 75, 0, Math.PI / 2, true);
-ctx.lineWidth = 20;
-ctx.strokeStyle = "green"; // !
-ctx.stroke();
-ctx.closePath();
+function animation( ctx, superman, fireball ) {
+  setInterval( () => {
+    clear( ctx );
+    background( ctx );
+    drwaComp( ctx, superman );
+    drwaComp( ctx, fireball );
+    fireball.posX -= 2;
 
-ctx.beginPath();
-ctx.arc(150, 170, 35, 0, Math.PI * 2);
-ctx.fillStyle = "red"; // !
-// fills the inner circle with red color
-ctx.fill();
-ctx.closePath();
+    if( fireball.posX <= -70 ) {
+      fireball.posX = 450;
+      fireball.posY = Math.floor(Math.random() * 430);
+    }
+  }, 1000/60 )
+}
+
+function background( ctx ) {  
+  ctx.fillStyle="lightblue";
+  ctx.fillRect(0, 0, 500, 500);
+}
+
+function drwaComp ( ctx, obj ) {
+  let img = new Image();
+  img.src = obj.imgScr;
+  ctx.drawImage(img, obj.posX, obj.posY, obj.imgW, obj.imgH);
+}
+
+function clear( ctx ) {
+  ctx.clearRect(0, 0, canva.width, canva.height)
+}
+
+
+// *************************************************** //
+
+window.addEventListener('load', () => {
+  init();
+});
+
+
